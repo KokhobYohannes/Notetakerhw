21c0908592a3183cc2b5dcdd7046e171c60fbcec
Kokhob Yohannes <kokhobyohannes@yahoo.com>
2021-02-05 00:05:29 -0500

    Updated route

--- route/note.js
+++ route/note.js
@@ -2,18 +2,18 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/util')
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res, next)=>{
 
     res.json(db.readAll())
 })
 
-router.post('/', (req, res)=>{
+router.post('/', (req, res, next)=>{
     let body = req.body
     db.addNote(body)
     res.json({"message": "note added successfully"})
 })
 
-router.delete('/', (req, res)=>{
+router.delete('/', (req, res, next)=>{
 
     res.json({"test": "answer"})
 })
